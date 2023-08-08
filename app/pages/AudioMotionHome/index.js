c3aefdc3a41262aa04b2dc6631808280cb00eddf
Joseph Francis <hookedupjoe@gmail.com>
2023-08-08 11:56:30 -0400

    init commit

---
+++ app/pages/AudioMotionHome/index.js
@@ -0,0 +1,98 @@
+(function (ActionAppCore, $) {
+
+    var SiteMod = ActionAppCore.module("site");
+
+    //~thisPageSpecs//~
+var thisPageSpecs = {
+	"pageName": "AudioMotionHome",
+	"pageTitle": "Audio Motion",
+	"navOptions": {
+		"topLink": true,
+		"sideLink": true
+	}
+}
+//~thisPageSpecs~//~
+
+    var pageBaseURL = 'app/pages/' + thisPageSpecs.pageName + '/';
+
+    //~layoutOptions//~
+thisPageSpecs.layoutOptions = {
+        baseURL: pageBaseURL,
+        north: false,
+        east: { html: 'east' },
+        west: false,
+        center: { control: "AudioMotionGlobal", source:"__app" },
+        south: false
+    }
+//~layoutOptions~//~
+
+    //~layoutConfig//~
+thisPageSpecs.layoutConfig = {
+        west__size: "500"
+        , east__size: "250"
+    }
+//~layoutConfig~//~
+    //~required//~
+thisPageSpecs.required = {
+
+    }
+//~required~//~
+
+    var ThisPage = new SiteMod.SitePage(thisPageSpecs);
+
+    var actions = ThisPage.pageActions;
+
+    ThisPage._onPreInit = function (theApp) {
+        //~_onPreInit//~
+
+//~_onPreInit~//~
+    }
+
+    ThisPage._onInit = function () {
+        //~_onInit//~
+
+//~_onInit~//~
+    }
+
+
+    ThisPage._onFirstActivate = function (theApp) {
+        //~_onFirstActivate//~
+
+//~_onFirstActivate~//~
+        ThisPage.initOnFirstLoad().then(
+            function () {
+                //~_onFirstLoad//~
+
+//~_onFirstLoad~//~
+                ThisPage._onActivate();
+            }
+        );
+    }
+
+
+    ThisPage._onActivate = function () {
+        //~_onActivate//~
+
+//~_onActivate~//~
+    }
+
+    ThisPage._onResizeLayout = function (thePane, theElement, theState, theOptions, theName) {
+        //~_onResizeLayout//~
+
+//~_onResizeLayout~//~
+    }
+
+    //------- --------  --------  --------  --------  --------  --------  -------- 
+    //~YourPageCode//~
+actions.micOn = micOn;
+function micOn(){
+  this.parts.center.micOn();
+}
+
+actions.micOff = micOff;
+function micOff(){
+  this.parts.center.micOff();
+}
+//~YourPageCode~//~
+
+})(ActionAppCore, $);
---
+++ catalog/controls/AudioMotionGlobal/control.js
@@ -0,0 +1,163 @@
+(function (ActionAppCore, $) {
+
+	var ControlSpecs = { 
+		options: {
+			padding: false
+		},
+		content: [
+		{
+			ctl: "spot",
+			name: "AudioMotionAnalyzerBody",
+			text: '<div id="containermic"></div>'
+		}
+		]
+	}
+
+	var ControlCode = {};
+
+  var currStream = false,
+     readyFlag = false,
+     audioMotion = false;
+     
+  
+  
+  
+  ControlCode.isReady = isReady;
+  function isReady() {
+    return readyFlag;  
+  }
+
+  ControlCode.initSetup = initSetup;
+  function initSetup() {
+
+    // instantiate analyzer
+    
+    audioMotion = audioMotion || new AudioMotionAnalyzer(
+      document.getElementById('containermic'),
+      {
+        gradient: 'rainbow',
+        height: window.innerHeight - 40,
+        showScaleY: true,
+        useCanvas: true,
+        onCanvasDraw: function(instance){
+          ThisApp.common.eqDataMic = {
+            bands: instance.getBars(),
+            peak: instance.getEnergy('peak'),
+            bass: instance.getEnergy('bass'),
+            lowMid: instance.getEnergy('lowMid'),
+            mid: instance.getEnergy('mid'),
+            highMid: instance.getEnergy('highMid'),
+            treble: instance.getEnergy('treble')
+          }
+        }
+
+      }
+    );
+    window.audioMotionInUse = audioMotion;
+    var tmpFrameEl = ThisApp.util.resizeToParent(audioMotion.canvas);
+    tmpFrameEl.css('overflow','hidden')
+    audioMotion.mode = 6;
+    readyFlag = true;
+
+  }
+
+
+ControlCode.micOn = micOn;
+  function micOn() {
+    console.log( 'micOn ctl');
+    if( !(readyFlag) ){
+      initSetup();
+    }
+    if ( navigator.mediaDevices ) {
+    navigator.mediaDevices.getUserMedia( {
+      audio: true, video: false
+    }).then(function(stream){
+        const micStream = audioMotion.audioCtx.createMediaStreamSource(stream);
+        audioMotion.connectInput(micStream);
+        audioMotion.volume = 0;
+      })
+      .catch(function(err){
+        console.error('Error accessing mic',err);
+      });
+
+    } else {
+      console.error('Mic not supported');      
+    }
+  }
+
+//-- ToDo: Load like three js
+  async function isAnalyserThere(){
+    var dfd = jQuery.Deferred();
+    ThisApp.delay(25).then(function(){
+      if( window.AudioMotionAnalyzer ){
+        dfd.resolve(true);
+      } else {
+        dfd.resolve(false);
+      }
+    })
+    return dfd.promise();
+  }
+  
+  async function waitForAnalyzer(){
+    var dfd = jQuery.Deferred();
+    for( var i = 0 ; i < 100 ; i++){
+        if(await isAnalyserThere()){
+          dfd.resolve(true)
+        }
+        if( i >= 90){
+          dfd.resolve(false);
+        }
+    }
+    return dfd.promise();
+  }
+  ControlCode.micOff = micOff;
+  function micOff() {
+    if( audioMotion ){
+      audioMotion.disconnectInput(false, true);
+    }
+  }
+
+
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ControlCode.setup = setup;
+    function setup(){
+        console.log("Ran setup")
+    }
+
+    ControlCode._onInit = _onInit;
+    function _onInit(){
+      window.AMG = this;
+        //waitForAnalyzer().then(initSetup)
+    }
+
+	var ThisControl = {specs: ControlSpecs, options: { proto: ControlCode, parent: ThisApp }};
+	return ThisControl;
+})(ActionAppCore, $);
